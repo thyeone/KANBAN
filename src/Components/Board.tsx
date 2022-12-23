e92f5c7f89b5de2d4efecb7126d35916586ab820
thyeone <thyeonee@gmail.com>
2022-12-23 21:02:23 +0900

    7.11 Droppable Snapshot

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -12,28 +12,42 @@ function Board({ toDos, boardId }: IBoardProps) {
     <Wrapper>
       <Title>{boardId}</Title>
       <Droppable droppableId={boardId}>
-        {(magic) => (
-          <BoardWrapper ref={magic.innerRef} {...magic.droppableProps}>
+        {(magic, info) => (
+          <Area
+            isDraggingOver={info.isDraggingOver}
+            isDraggingFromThisWith={Boolean(info.draggingFromThisWith)}
+            ref={magic.innerRef}
+            {...magic.droppableProps}
+          >
             {toDos.map((toDo, idx) => (
               <DraggableCard key={toDo} toDo={toDo} index={idx} />
             ))}
             {magic.placeholder}
-          </BoardWrapper>
+          </Area>
         )}
       </Droppable>
     </Wrapper>
   );
 }
 
-const BoardWrapper = styled.div`
-  padding: 20px 10px;
-  padding-top: 30px;
-  background-color: ${(props) => props.theme.boardColor};
-  border-radius: 5px;
-  min-height: 200px;
+interface IAreaProps {
+  isDraggingOver: boolean;
+  isDraggingFromThisWith: boolean;
+}
+const Area = styled.div<IAreaProps>`
+  background-color: ${(props) =>
+    props.isDraggingOver
+      ? "pink"
+      : props.isDraggingFromThisWith
+      ? "red"
+      : "blue"};
+  flex-grow: 1;
+  transition: background-color 0.3s ease-in-out;
 `;
 
 const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
   width: 300px;
   padding: 20px 10px;
   padding-top: 10px;
