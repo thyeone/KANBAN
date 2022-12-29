3a9be9fd31570f3027171a8c22d8d3360e9cc38d
thyeone <thyeonee@gmail.com>
2022-12-29 21:42:22 +0900

    Delete Task and Board, Create new board, localStorage, Moving Boards

--- src/Pages/App.tsx
+++ src/Pages/App.tsx
@@ -1,8 +1,7 @@
 import { DragDropContext, Droppable, DropResult } from "react-beautiful-dnd";
-import { useForm } from "react-hook-form";
-import { useRecoilState, useSetRecoilState } from "recoil";
+import { useRecoilState } from "recoil";
 import styled from "styled-components";
-import { boardState, deleteState, toDoState } from "../atoms";
+import { boardState, toDoState } from "../atoms";
 import Board from "../Components/Board";
 import CreateBoard from "../Components/CreateBoard";
 import TrashCan from "../Components/TrashCan";
@@ -10,23 +9,29 @@ import TrashCan from "../Components/TrashCan";
 function App() {
   const [toDos, setToDos] = useRecoilState(toDoState);
   const [boards, setBoards] = useRecoilState(boardState);
-  const trashCan = useSetRecoilState(deleteState);
 
   const onDragEnd = (info: DropResult) => {
     console.log(info);
     const { destination, source } = info;
     if (!destination) return;
-    trashCan(true);
-    if (source.droppableId === "boards") {
-      setBoards((prev) => {
-        const boardCopy = [...prev];
+    if (destination.droppableId === "trashcan") {
+      setToDos((allBoards) => {
+        const sourceBoard = [...allBoards[source.droppableId]];
+        sourceBoard.splice(source.index, 1);
+        return {
+          ...allBoards,
+          [source.droppableId]: sourceBoard,
+        };
+      });
+    } else if (source.droppableId === "boards") {
+      setBoards((allBoards) => {
+        const boardCopy = [...allBoards];
         const item = boardCopy[source.index];
         boardCopy.splice(source.index, 1); // 지우고
         boardCopy.splice(destination.index, 0, item); // 도착지의 인덱스에 item을 추가
         return boardCopy;
       });
-    }
-    if (destination?.droppableId === source.droppableId) {
+    } else if (destination?.droppableId === source.droppableId) {
       // same board movement.
       setToDos((allBoards) => {
         const boardCopy = [...allBoards[source.droppableId]];
@@ -38,8 +43,7 @@ function App() {
           [source.droppableId]: boardCopy,
         };
       });
-    }
-    if (destination.droppableId !== source.droppableId) {
+    } else if (destination.droppableId !== source.droppableId) {
       // cross board movement
       setToDos((allBoards) => {
         const sourceBoard = [...allBoards[source.droppableId]];
